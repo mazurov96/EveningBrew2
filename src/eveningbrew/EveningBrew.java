80bd5c50285af43e5b3fb74a405d8c0e02e029c8
Nikita <Nikita@10.1.7.93>
2017-05-01 22:44:06 +0200

    Просмотр наименований пива

--- src/eveningbrew/EveningBrew.java
+++ src/eveningbrew/EveningBrew.java
@@ -5,6 +5,12 @@
  */
 package eveningbrew;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author User
@@ -15,7 +21,49 @@ public class EveningBrew {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here2281488
+         // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://localhost:3306/new_schema";
+    private static final String user = "root";
+    private static final String password = "";
+    
+    // JDBC variables for opening and managing connection
+    private static Connection con;
+    private static Statement stmt;
+    private static ResultSet rs;
+   
+
+  public static void main(String args[]) {
+        String pop="mazda6";
+        String query = "select Name_Beer from beer_main";
+              
+ 
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection(url, user, password);
+ 
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+ 
+            // executing SELECT query
+            stmt.executeQuery(query);
+ 
+            while (rs.next()) {
+                String name = rs.getString(2);
+                System.out.println("Total number of books in the table : " + name);
+            }
+ 
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } finally {
+            //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            //try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+        
+    
+
+    }
     }
     
 }
