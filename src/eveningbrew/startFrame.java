e8c6a288230db61af249967d19f0ce1b3b1a2a7d
User <User@Asus>
2017-05-02 02:59:26 +0300

    Добавил фотку

--- src/eveningbrew/startFrame.java
+++ src/eveningbrew/startFrame.java
@@ -29,6 +29,10 @@ public class startFrame extends javax.swing.JFrame {
 
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -37,15 +41,52 @@ public class startFrame extends javax.swing.JFrame {
 
         jPanel2.setLayout(null);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 255, 0));
         jLabel2.setText("Главное меню");
         jPanel2.add(jLabel2);
-        jLabel2.setBounds(40, 20, 150, 60);
+        jLabel2.setBounds(80, 30, 280, 170);
+
+        jButton2.setBackground(new java.awt.Color(51, 51, 255));
+        jButton2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 51, 51));
+        jButton2.setText("О приложении");
+        jButton2.setActionCommand("Начать работу");
+        jPanel2.add(jButton2);
+        jButton2.setBounds(390, 430, 230, 110);
+
+        jButton6.setBackground(new java.awt.Color(51, 51, 255));
+        jButton6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton6.setForeground(new java.awt.Color(255, 51, 51));
+        jButton6.setText("Выход");
+        jButton6.setActionCommand("Начать работу");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton6);
+        jButton6.setBounds(390, 540, 230, 110);
+
+        jButton7.setBackground(new java.awt.Color(51, 51, 255));
+        jButton7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton7.setForeground(new java.awt.Color(255, 51, 51));
+        jButton7.setText("Показ карты города");
+        jButton7.setActionCommand("Начать работу");
+        jPanel2.add(jButton7);
+        jButton7.setBounds(390, 310, 230, 120);
+
+        jButton8.setBackground(new java.awt.Color(51, 51, 255));
+        jButton8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButton8.setForeground(new java.awt.Color(255, 51, 51));
+        jButton8.setText("Начать работу");
+        jButton8.setActionCommand("Начать работу");
+        jPanel2.add(jButton8);
+        jButton8.setBounds(390, 200, 230, 110);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/foto/pivo2.jpg"))); // NOI18N
         jPanel2.add(jLabel1);
-        jLabel1.setBounds(1, 0, 650, 360);
+        jLabel1.setBounds(-90, -40, 1200, 800);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -60,6 +101,11 @@ public class startFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+      System.exit(0);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton6ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -97,6 +143,10 @@ public class startFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel2;
